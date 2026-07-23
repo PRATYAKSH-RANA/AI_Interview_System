57d715a140fc3b8a3998c9269da25222f182e9f3
PRATYAKSH-RANA <honezrana123@gmail.com>
2026-07-23 15:38:24 +0530

    half-home-bar-done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import { useSelector } from 'react-redux'
 import { motion } from "motion/react"
+import { useState } from 'react'
+import AuthModel from '../components/AuthModel'
 import{
   BsRobot,
   BsCoin,
   BsClock,
   BsBarChart,
-  BsFileEarmarkText
+  BsFileEarmarkText,
+  BsMic
 } from "react-icons/bs"
 import { HiSparkles } from 'react-icons/hi'
+import { useNavigate } from 'react-router-dom'
+
+
 const Home = () => {
   const {userData}=useSelector((state)=>state.user)
+  const[showAuth,setShowAuth]=useState(false)
+  const navigate= useNavigate()
   return (
     <div className='min-h-screen bg-[#f3f3f3] flex flex-col'>
       <Navbar />
@@ -42,9 +50,101 @@ const Home = () => {
                          className='text-gray-500 mt-6 max-w-2xl mx-auto text-lg'>
                           Role-Based Mock Interviews with AI Powered Intelligence with smart follow-ups,adaptive difficulty and real time feedback.
                         </motion.p>
+                        <div className='flex flex-wrap justify-center gap-4 mt-10'>
+                           <motion.button
+                           onClick={()=>{
+                               if (!userData) {
+                                   setShowAuth(true)
+                                   return;
+                               }
+                               navigate("/interview")
+                           }}
+                           initial={{opacity:0,y:30}}
+                           animate={{opacity:1,y:0}}
+                           whileHover={{ opacity:0.9 ,scale: 1.03 }}
+                           whileTap={{ opacity:1 ,scale: 1.03 }}
+                           className='bg-black text-white px-10 py-3 rounded-full hover:opacity-90 transition shadow-md'>
+                            Start Interview
+                           </motion.button>
+
+                           <motion.button
+                           onClick={()=>{
+                               if (!userData) {
+                                   setShowAuth(true)
+                                   return;
+                               }
+                               navigate("/history")
+                           }}
+                           initial={{opacity:0,y:30}}
+                           animate={{opacity:1,y:0}}
+                           whileHover={{ opacity:0.9 ,scale: 1.03 }}
+                           whileTap={{ opacity:1 ,scale: 1.03 }}
+                           className='border border-gray-300 px-10 py-3 rounded-full hover:bg-gray-100 transition'>
+                            View History
+                           </motion.button>
+
+                        </div>
                          
                  </div>
+                    <div className='flex flex-col md:flex-row justify-center items-center gap-10 mb-28'>
+                        {
+                          [
+                            {
+                              icon: <BsRobot size={24} />,
+                              step: "Step 1",
+                              title: "Role and Experience selection",
+                              description: "Select the type of interview you want to do"
+                            },
+                            {
+                              icon: <BsMic size={24} />,
+                              step: "Step 2",
+                              title: "Smart Voice Assistant",
+                              description: "Follow ups questions based on your answers"
+                            },
+                            {
+                              icon: <BsClock size={24} />,
+                              step: "Step 3",
+                              title: "Time based Interview",
+                              description: "Real interview time"
+                            },
+                            
+                          ].map((item, index) => (
+                            <motion.div key={index} 
+                             initial={{opacity:0, y:60}}
+                             whileInView={{ opacity:1,y:0}}
+                             transition={{duration:0.6 + index * 0.2}}
+                             whileHover={{ rotate: 0 , scale: 1.06 }}
+                             className={
+                             `relative bg-white rounded-3xl border-2 border-green-100 hover:border-green-500 p-10 w-80 max-w-[90%] shadow-md hover:shadow-2xl transition-all duration-300 ${index === 0 ? "rotate-[-4deg]" : ""} ${index === 1 ? "rotate-[-3deg] md:-mt-6 shadow-xl" : ""} ${index === 2 ? "rotate-[-3deg]" : ""}`
+                            }>
+                                <div className='absolute -top-8 left-1/2 -translate-x-1/2 bg-white border-2 border-green-500 text-green-600 w-16 h-16 rounded-2xl flex items-center justify-center shadow-lg'>
+                                  {item.icon}
+                                </div>
+                                <div className='pt-10 text-center'>
+                                  <div className='text-xs text-green-600 font-semibold mb-2 tracking-wider'>
+                                    {item.step}
+                                  </div>
+                                      <h3 className='font-semibold mb-3 text-lg'>{item.title}</h3>
+                                      <p className='text-sm text-gray-500 leading-relaxed'>{item.description}</p>
+                                  
+                                </div>
+                             </motion.div>
+                        ))
+                      }
+                   </div>
+                   <div className='mb-32'>
+                    <motion.h2
+                     initial={{opacity:0, y:20}}
+                     whileInView={{opacity:1,y:0}}
+                     transition={{duration:0.6}}
+                     className='text-4xl font-semibold text-center mb-16'>
+                       Advanced AI{" "}
+                       <span className='text-green-600'>Capabilities</span>
+                    </motion.h2>
+
+                   </div>
       </div>
+      {showAuth && <AuthModel onClose={()=>setShowAuth(false)}/>}
     </div>
   )
 }
